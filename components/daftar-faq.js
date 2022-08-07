efd3234c3b10376f999a2da87435640e05de512a
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-08-07 22:20:39 +0700

    add daftar FAQ section

---
+++ components/daftar-faq.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const styleQ = "w-full px-[24px] py-[25px] rounded bg-grey-100 font-Josefin font-bold text-xl"
+const styleA = "font-Josefin font-light text-xl pb-10"
+
+export default function DaftarFAQ () {
+    return (
+        <div className="w-full h-full grid content-center px-16 py-[100px] bg-white">
+            <div className="container mx-auto space-y-5">
+                <div className={styleQ}>
+                    <h2>Q. What is the name of department</h2>
+                </div>
+                <p className={styleA}>A. Lorem ipsum dolor sit amet, consecte adipiscing elit. Donec hendrerit metus enim, quis pretm odio vehicula eget. Aliq quis iaclis mi, a laoreet elit. Fus neque dui, vestibulum at consectetur eu, mie vel mag. Pellentesque ac ipsum facilisis, pharetra ex non, efficitur felis.</p>
+                <div className={styleQ}>
+                    <h2>Q. What is the name of department</h2>
+                </div>
+                <p className={styleA}>A. Lorem ipsum dolor sit amet, consecte adipiscing elit. Donec hendrerit metus enim, quis pretm odio vehicula eget. Aliq quis iaclis mi, a laoreet elit. Fus neque dui, vestibulum at consectetur eu, mie vel mag. Pellentesque ac ipsum facilisis, pharetra ex non, efficitur felis.</p>
+                <div className={styleQ}>
+                    <h2>Q. What is the name of department</h2>
+                </div>
+                <p className={styleA}>A. Lorem ipsum dolor sit amet, consecte adipiscing elit. Donec hendrerit metus enim, quis pretm odio vehicula eget. Aliq quis iaclis mi, a laoreet elit. Fus neque dui, vestibulum at consectetur eu, mie vel mag. Pellentesque ac ipsum facilisis, pharetra ex non, efficitur felis.</p>
+                <div className={styleQ}>
+                    <h2>Q. What is the name of department</h2>
+                </div>
+                <p className={styleA}>A. Lorem ipsum dolor sit amet, consecte adipiscing elit. Donec hendrerit metus enim, quis pretm odio vehicula eget. Aliq quis iaclis mi, a laoreet elit. Fus neque dui, vestibulum at consectetur eu, mie vel mag. Pellentesque ac ipsum facilisis, pharetra ex non, efficitur felis.</p>
+                <div className={styleQ}>
+                    <h2>Q. What is the name of department</h2>
+                </div>
+                <p className={styleA}>A. Lorem ipsum dolor sit amet, consecte adipiscing elit. Donec hendrerit metus enim, quis pretm odio vehicula eget. Aliq quis iaclis mi, a laoreet elit. Fus neque dui, vestibulum at consectetur eu, mie vel mag. Pellentesque ac ipsum facilisis, pharetra ex non, efficitur felis.</p>
+            </div>
+        </div>
+    )
+}
--- pages/faq.js
+++ pages/faq.js
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import DaftarFAQ from '../components/daftar-faq';
 
 export default function FAQ() {
     return(
         <>
             <Navbar />
+            <DaftarFAQ />
         </>
     )
 }
