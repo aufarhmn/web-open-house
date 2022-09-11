80a6d5041b8061de9dec23109c57bccef4c32de2
Haikal Hilmi <haikal6255hilmi@gmail.com>
2022-09-12 01:23:50 +0700

    feat : FAQ JSON

--- components/DaftarFAQ.js
+++ components/DaftarFAQ.js
@@ -1,4 +1,5 @@
 import React from "react";
+import faq from "./FAQ.json";
 
 const styleQ = "w-full px-[24px] py-[25px] rounded bg-grey-100 font-Josefin font-bold text-xl"
 const styleA = "font-Josefin font-light text-xl pb-10"
@@ -7,26 +8,19 @@ export default function DaftarFAQ () {
     return (
         <div className="w-full h-full grid content-center px-16 py-[100px] bg-white">
             <div className="container mx-auto space-y-5">
-                <div className={styleQ}>
-                    <h2>Q. What is the name of department</h2>
-                </div>
-                <p className={styleA}>A. Lorem ipsum dolor sit amet, consecte adipiscing elit. Donec hendrerit metus enim, quis pretm odio vehicula eget. Aliq quis iaclis mi, a laoreet elit. Fus neque dui, vestibulum at consectetur eu, mie vel mag. Pellentesque ac ipsum facilisis, pharetra ex non, efficitur felis.</p>
-                <div className={styleQ}>
-                    <h2>Q. What is the name of department</h2>
-                </div>
-                <p className={styleA}>A. Lorem ipsum dolor sit amet, consecte adipiscing elit. Donec hendrerit metus enim, quis pretm odio vehicula eget. Aliq quis iaclis mi, a laoreet elit. Fus neque dui, vestibulum at consectetur eu, mie vel mag. Pellentesque ac ipsum facilisis, pharetra ex non, efficitur felis.</p>
-                <div className={styleQ}>
-                    <h2>Q. What is the name of department</h2>
-                </div>
-                <p className={styleA}>A. Lorem ipsum dolor sit amet, consecte adipiscing elit. Donec hendrerit metus enim, quis pretm odio vehicula eget. Aliq quis iaclis mi, a laoreet elit. Fus neque dui, vestibulum at consectetur eu, mie vel mag. Pellentesque ac ipsum facilisis, pharetra ex non, efficitur felis.</p>
-                <div className={styleQ}>
-                    <h2>Q. What is the name of department</h2>
-                </div>
-                <p className={styleA}>A. Lorem ipsum dolor sit amet, consecte adipiscing elit. Donec hendrerit metus enim, quis pretm odio vehicula eget. Aliq quis iaclis mi, a laoreet elit. Fus neque dui, vestibulum at consectetur eu, mie vel mag. Pellentesque ac ipsum facilisis, pharetra ex non, efficitur felis.</p>
-                <div className={styleQ}>
-                    <h2>Q. What is the name of department</h2>
-                </div>
-                <p className={styleA}>A. Lorem ipsum dolor sit amet, consecte adipiscing elit. Donec hendrerit metus enim, quis pretm odio vehicula eget. Aliq quis iaclis mi, a laoreet elit. Fus neque dui, vestibulum at consectetur eu, mie vel mag. Pellentesque ac ipsum facilisis, pharetra ex non, efficitur felis.</p>
+                {
+                    //TODO json faq map()
+                    faq["faq"].map((e) => {
+                        return (
+                            <>
+                                <div className={styleQ}>
+                                    <h2>{e.ask}</h2>
+                                </div>
+                                <p className={styleA}>{e.answer}</p>
+                            </>
+                        );
+                    })}
+
             </div>
         </div>
     )
