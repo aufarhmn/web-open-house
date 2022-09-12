01bc699afeaf6daf5f6e8284839a0d9de4952b21
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-09-12 14:35:28 +0700

    add About section

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import DeanMessage from '../components/DeanMessage'
 import SubEvents from '../components/SubEvents'
 import BackToTop from '../components/BackToTop'
 import OHEvents from '../components/OHEvents'
+import About from '../components/About'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       {/* Form belum diexport */}
       <Navbar />
       <Hero />
+      <About />
       <OHEvents />
       <DeanMessage />
       <SubEvents />
