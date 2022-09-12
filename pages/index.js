f75d076c0aecfd8e22413c5b56d5f38ed0fcbcc7
NandoAACF <nando.aacf88@gmail.com>
2022-09-12 15:13:55 +0700

    feat: done location and footer

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,24 @@
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
-import DeanMessage from '../components/DeanMessage'
-import SubEvents from '../components/SubEvents'
-import BackToTop from '../components/BackToTop'
-import OHEvents from '../components/OHEvents'
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import DeanMessage from "../components/DeanMessage";
+import SubEvents from "../components/SubEvents";
+import BackToTop from "../components/BackToTop";
+import OHEvents from "../components/OHEvents";
+import Footer from "../components/Footer";
+import Location from "../components/Location";
 
 export default function Home() {
-  return (
-    <>
-      {/* Form belum diexport */}
-      <Navbar />
-      <Hero />
-      <OHEvents />
-      <DeanMessage />
-      <SubEvents />
-      <BackToTop />
-    </>
-  )
+    return (
+        <>
+            {/* Form belum diexport */}
+            <Navbar />
+            <Hero />
+            <OHEvents />
+            <Location />
+            <DeanMessage />
+            <SubEvents />
+            <Footer />
+            <BackToTop />
+        </>
+    );
 }
