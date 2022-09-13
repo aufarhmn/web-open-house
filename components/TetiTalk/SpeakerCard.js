7a7df2d13da4ce37861168f6facdf71c812f8edd
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-09-13 20:34:10 +0700

    feat: add speakers and benefits to TetiTalk

---
+++ components/TetiTalk/SpeakerCard.js
@@ -0,0 +1,58 @@
+import Image from "next/image";
+
+export const SpeakerCard = (props) => {
+  const benefitsStyle =
+    "bg-[#F3F3F3] rounded-[28px] w-[430px] h-[592px] flex flex-col items-center ";
+  const imgStyle =
+    "w-[100%] h-[100%] flex justify-center items-end rounded-t-[28px]";
+  const shadow = {
+    boxShadow:
+      "-13.1635px -13.1635px 32.9088px #FFFFFF, 13.1635px 13.1635px 32.9088px #D2D2D2",
+  };
+
+  let flexDir = "w-[full] flex my-3 gap-32 justify-between";
+
+  if(props.index % 2 == 1) {
+    flexDir = "w-[full] flex my-3 gap-32 justify-between flex-row";
+  } else {
+    flexDir = "w-[full] flex my-3 gap-32 justify-between flex-row-reverse";
+  }  
+
+  return (
+    <div className="w-[80%] mx-auto mb-32">
+      <div className={flexDir}>
+        <div className={benefitsStyle} style={shadow}>
+          <div className={imgStyle}>
+            <Image
+              src={props.photo}
+              alt="advantage1"
+              width={400}
+              height={300}
+            />
+          </div>
+          <div className="h-[35%] w-[80%] flex flex-col justify-center">
+            <p className="font-Josefin text-[26px] text-[#434343] font-bold">
+              {props.name}
+            </p>
+            <p className="font-Josefin text-[26px] text-[#434343]">
+              {props.job}
+            </p>
+          </div>
+        </div>
+        <div className="max-w-[646px] font-Poppins flex flex-col justify-evenly">
+          <h2 className="text-[32px] font-bold">{props.name}</h2>
+          <p className="text-[24.22px] mt-10">{props.desc}</p>
+          <p className="text-[24.22px] mt-10">
+            Best Paper Award, IEEE Indonesian Section, 2015 Best Paper Award,
+            Robotics Society of Taiwan, 2013
+          </p>
+          <div className="flex w-full gap-2">
+            <div className="bg-gray-500 h-[50px] w-full rounded"></div>
+            <div className="bg-gray-500 h-[50px] w-full rounded"></div>
+            <div className="bg-gray-500 h-[50px] w-full rounded"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- pages/tetitalk.js
+++ pages/tetitalk.js
@@ -1,8 +1,33 @@
 import Head from "next/head";
 import NavbarSimplified from "../components/NavbarSimplified";
+import Benefits from "../components/Benefits";
+import { SpeakerCard } from "../components/TetiTalk/SpeakerCard";
+
 
 export default function Tetitalk() {
   //TODO: Benefits Section, Speaker Section, Responsiveness, Footer, Navbar
+
+  const speakers = [
+    {
+      index: 1,
+      name: "Adha Imam Cahyadi",
+      job: "Control for nonlinear dynamical systems, Control for systems with delay, Multi agents control systems, Teleoperation systems",
+      photo: "/vercel.svg",
+    },
+    {
+      index: 2,
+      name: "Adha Imam Cahyadi 2",
+      job: "Control for nonlinear dynamical systems, Control for systems with delay, Multi agents control systems, Teleoperation systems",
+      photo: "/vercel.svg",
+    },
+    {
+      index: 3,
+      name: "Adha Imam Cahyadi 3",
+      job: "Control for nonlinear dynamical systems, Control for systems with delay, Multi agents control systems, Teleoperation systems",
+      photo: "/vercel.svg",
+    },
+  ]
+
   return (
     <>
       <Head>
@@ -10,14 +35,21 @@ export default function Tetitalk() {
       </Head>
       <main>
         <NavbarSimplified />
-        <h1 className="text-[134px] pl-32 font-Josefin font-bold mt-[130px] text-blue-25">
+        <h1 className="text-[134px] font-Josefin font-bold my-[120px] mx-[100px] text-blue-25">
           TETI Talk
         </h1>
 
-        <div className="flex mt-[240px] px-32 font-Josefin justify-between gap-4">
-          <div className="font-bold">
+        <div className="flex px-32 font-Josefin justify-between gap-4 text-green-200">
+          <div className="font-bold text-center">
             <h2 className="text-[58.5px] w-max">About TETI</h2>
-            <h2 className="text-[72.65px] bg-[#E7FFC1]">Course</h2>
+            <h2
+              className="text-[72.65px] bg-[#E7FFC1]"
+              style={{
+                clipPath: "polygon(10% 0%, 100% 0%, 90% 100%, 0% 100%)",
+              }}
+            >
+              Talk
+            </h2>
           </div>
           <p className="max-w-[730px] text-[30px] font-light">
             A. Lorem ipsum dolor sit amet, consecte adipiscing elit. Donec
@@ -30,8 +62,17 @@ export default function Tetitalk() {
           </p>
         </div>
 
-        <div className="p-32 mt-20">
-          <h1 className="text-[58.5px] font-bold font-Josefin">The Benefits</h1>
+        <Benefits />
+
+        <div className="mx-auto w-[80%]">
+          <h1 className="text-[58.5px] font-Poppins font-bold text-green-200 mb-32">
+            Meet Our Speakers
+          </h1>
+        </div>
+        <div className="odd:flex-row-reverse">
+          {speakers.map((speaker) => (
+            <SpeakerCard index={speaker.index} photo={speaker.photo} name={speaker.name} job="Dosen" desc={speaker.job} />
+          ))}
         </div>
       </main>
     </>
