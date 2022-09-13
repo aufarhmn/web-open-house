8dd1ba19384b407ee9250d881553c6eb02f016ff
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-09-13 19:14:17 +0700

    fix: small fixes

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-//TODO : Responsiveness, button links, animation, hover
+//TODO : Responsiveness, animation, hover
 
 const scrollToForm = () => {
   const form = document.getElementById("form");
@@ -15,7 +15,7 @@ const scrollToEvents = () => {
 const Hero = () => {
   return (
     <div className="flex flex-col items-center font-Poppins pt-10">
-      <div className="px-16 md:leading-[85px] leading-[64px] w-full text-white flex items-center justify-between flex-col lg:flex-row">
+      <div className="px-[5%] md:leading-[85px] leading-[64px] w-full text-white flex items-center justify-between flex-col lg:flex-row">
         <div data-aos="fade-up" data-aos-duration="700">
           <h1 className="md:text-[81px] text-[64px] max-w-[600px] font-bold text-blue-25">
             We look forward for you to join us!
@@ -25,7 +25,7 @@ const Hero = () => {
           <Image src="/OpenHouseHero.png" width={3147} height={2000} />
         </div>
       </div>
-      <div className="w-full px-16 flex gap-5 lg:gap-16 md:flex-row text-[23px] justify-center items-center flex-col lg:justify-start md:mt-5">
+      <div className="w-full px-[5%] flex gap-5 lg:gap-16 md:flex-row text-[23px] justify-center items-center flex-col lg:justify-start md:mt-5">
         <div
           className="cursor-pointer text-white bg-[#F0ABC0] flex justify-center items-center w-[80%] md:w-[192px] h-[85px] rounded-[15px] shadow-lg"
           onClick={() => {
