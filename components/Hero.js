3f157257b718797ecdb4b7485b104177ac5c4e0a
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-09-13 19:09:30 +0700

    feat: add ability to scroll from Hero buttons

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,17 @@
 import Image from "next/image";
 
 //TODO : Responsiveness, button links, animation, hover
+
+const scrollToForm = () => {
+  const form = document.getElementById("form");
+  form.scrollIntoView({ behavior: "smooth" });
+};
+
+const scrollToEvents = () => {
+  const events = document.getElementById("events");
+  events.scrollIntoView({ behavior: "smooth" });
+}
+
 const Hero = () => {
   return (
     <div className="flex flex-col items-center font-Poppins pt-10">
@@ -11,12 +22,25 @@ const Hero = () => {
           </h1>
         </div>
         <div className="max-w-[580px] w-full">
-          <Image src="/OpenHouseHero.png" width={3147} height={2000}/>
+          <Image src="/OpenHouseHero.png" width={3147} height={2000} />
         </div>
       </div>
       <div className="w-full px-16 flex gap-5 lg:gap-16 md:flex-row text-[23px] justify-center items-center flex-col lg:justify-start md:mt-5">
-        <div className="cursor-pointer text-white bg-[#F0ABC0] flex justify-center items-center w-[80%] md:w-[192px] h-[85px] rounded-[15px] shadow-lg">Register Now</div>
-        <div className="cursor-pointer text-[#8A8D8F] bg-[#F9F9F9] flex justify-center items-center w-[80%] md:w-[192px] h-[85px] rounded-[15px] border-[5px] border-[#E3E3E3]">Learn More</div>
+        <div
+          className="cursor-pointer text-white bg-[#F0ABC0] flex justify-center items-center w-[80%] md:w-[192px] h-[85px] rounded-[15px] shadow-lg"
+          onClick={() => {
+            scrollToForm();
+          }}
+        >
+          Register Now
+        </div>
+        <div className="cursor-pointer text-[#8A8D8F] bg-[#F9F9F9] flex justify-center items-center w-[80%] md:w-[192px] h-[85px] rounded-[15px] border-[5px] border-[#E3E3E3]"
+          onClick={() => {
+            scrollToEvents();
+          }}
+        >
+          Learn More
+        </div>
       </div>
     </div>
   );
