c90f59bb83fe51e6f5892864e4e97a78a69ece9f
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-09-20 20:44:19 +0700

    feat: preparation for FormPopUp

---
+++ components/FormPopUp.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export const FormPopUp = (props) => {
+  // const [isOpen, setIsOpen] = useState(false);
+  // const [message, setMessage] = useState("");
+
+  return(
+    <div>
+      {/* show message here */}
+    </div>
+  )
+}
