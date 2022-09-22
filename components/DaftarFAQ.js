a074acabdee4024f40250b1c0a90125c7e0db9b9
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2022-09-22 21:14:52 +0700

    feat: fetch faq from firebase

--- components/DaftarFAQ.js
+++ components/DaftarFAQ.js
@@ -1,26 +1,30 @@
 import React from "react";
+import { db } from "../firebase/firebase.js";
+import { collection, getDocs } from "firebase/firestore";
 import faq from "./FAQ.json";
 
 const styleQ = "w-full px-[24px] py-[25px] rounded bg-grey-100 font-Josefin font-bold text-xl"
 const styleA = "font-Josefin font-light text-xl pb-10"
+
+const faqRef = collection(db, "faq-details");
+const docsSnap = await getDocs(faqRef);
+var data = docsSnap.docs.map((doc) => doc.data());
 
 export default function DaftarFAQ () {
     return (
         <div className="w-full h-full grid content-center px-16 py-[100px] bg-white">
             <div className="container mx-auto space-y-5">
                 {
-                    //TODO json faq map()
-                    faq["faq"].map((e) => {
+                    data.map((faq, index) => {
                         return (
                             <>
-                                <div className={styleQ}>
-                                    <h2>{e.ask}</h2>
+                                <div className={styleQ} key={index}>
+                                    <h2>{faq.question}</h2>
                                 </div>
-                                <p className={styleA}>{e.answer}</p>
+                                    <p className={styleA} key={index}>{faq.answer}</p>
                             </>
-                        );
-                    })}
-
+                    );
+                })}
             </div>
         </div>
     )
