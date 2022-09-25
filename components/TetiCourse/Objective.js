ca14cf2842ba0b7e316b34a79c7eb1c959b60ecc
NandoAACF <nando.aacf88@gmail.com>
2022-09-25 20:34:28 +0700

    feat: done teti course objective

---
+++ components/TetiCourse/Objective.js
@@ -0,0 +1,21 @@
+import { useEffect, useMemo, useState } from "react";
+import { IoArrowUpCircleSharp } from "react-icons/io5";
+import { BsFillArrowUpSquareFill, BsArrowUpSquareFill } from "react-icons/bs";
+import clsx from "clsx";
+import Image from "next/image";
+
+export default function Objective({
+    imgSrc = "",
+    body = "Introducing to you a UI & UX focused website with a cutting edge technology as a platform for digital marketing and lead generation.",
+}) {
+    return (
+        <>
+            <div className="flex flex-col md:flex-row items-center justify-center md:justify-start gap-[20px] md:gap-[88px] mt-[40px] md:mt-[30px]">
+                <div className="relative w-[144px] h-[144px]">
+                    <Image src={imgSrc} layout="fixed"></Image>
+                </div>
+                <h6 className="text-[24px] text-[#1F1F1F] md:text-left text-center">{body}</h6>
+            </div>
+        </>
+    );
+}
--- pages/teticourse.js
+++ pages/teticourse.js
@@ -3,6 +3,11 @@ import NavbarSimplified from "../components/NavbarSimplified";
 import BackToTop from "../components/BackToTop";
 import GetInTouch from "../components/GetInTouch";
 import Head from "next/head";
+import podium from "../Assets/Images/TetiCourse/podium.png";
+import love from "../Assets/Images/TetiCourse/love.png";
+import orang from "../Assets/Images/TetiCourse/orang.png";
+import Image from "next/image";
+import Objective from "../components/TetiCourse/Objective";
 
 export default function TETICourse() {
     return (
@@ -16,17 +21,47 @@ export default function TETICourse() {
             </div>
             <div className="flex w-full">
                 <div className="w-[40%] flex justify-center items-start">
-                    <h2 className="text-[58px] font-bold font-Josefin">About TETI
-                        <br/>
-                        <div className="w-[250px] h-[80px] bg-[#E7FFC1]" style={{clipPath: "polygon(10% 0%, 100% 0%, 90% 100%, 0% 100%)"}}>
+                    <h2 className="text-[58px] font-bold font-Josefin">
+                        About TETI
+                        <br />
+                        <div
+                            className="w-[250px] h-[80px] bg-[#E7FFC1]"
+                            style={{ clipPath: "polygon(10% 0%, 100% 0%, 90% 100%, 0% 100%)" }}
+                        >
                             <p className="text-center">Course</p>
                         </div>
                     </h2>
                 </div>
                 <div className="w-[60%] font-Poppins text-[30px] text-justify">
-                    <p className="w-[90%]">Rangkaian acara berupa <i>coding session</i> dengan beberapa pilihan course yang dapat dipilih. Beberapa course yang ditawarkan yakni UI/UX, Website Development, dan Robotika.</p>
+                    <p className="w-[90%]">
+                        Rangkaian acara berupa <i>coding session</i> dengan beberapa pilihan course yang dapat dipilih.
+                        Beberapa course yang ditawarkan yakni UI/UX, Website Development, dan Robotika.
+                    </p>
                 </div>
             </div>
+            <section className="objective">
+                <div className="flex flex-col items-center md:items-start justify-start overflow-hidden relative px-[40px] md:px-[100px] lg:px-[150px] pt-[130px] pb-[90px]">
+                    <h4 className="text-[56px] font-bold text-[#1F1F1F] md:text-left text-center">
+                        TETI Course Objective
+                    </h4>
+                    <Objective
+                        imgSrc={podium}
+                        body="Introducing to you a UI & UX focused website with a cutting edge technology as a platform for
+                            digital marketing and lead generation."
+                    />
+                    <Objective
+                        imgSrc={love}
+                        body="Establishing the first stepping stone in the micro markets of Navi Mumbai, and later leaving
+                        footprints as a standard bearer in the micro markets of Mumbai. We strive to always have a
+                        dedicated focus on the consumer's requirements and expectations"
+                    />
+                    <Objective
+                        imgSrc={orang}
+                        body="We believe in long term orientation with our diversified business model thus making us stand out
+                        in the industry"
+                    />
+                </div>
+            </section>
             <GetInTouch />
             <Footer />
             <BackToTop />
