daeedaae808bb33f7c0a0b1c49c7d5782d7812c3
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-09-26 19:18:57 +0700

    feat: FormPopUp update
    - added icons
    - responsive

--- components/FormPopUp.js
+++ components/FormPopUp.js
@@ -1,11 +1,14 @@
 import { db } from "../firebase/firebase";
 import { collection, getDocs } from "firebase/firestore";
+import { FaCheckCircle } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+import { IoMdCloseCircle } from "react-icons/io";
 
 var data;
 const getInv = await getDocs(collection(db, "links"));
-getInv.forEach(doc => {
+getInv.forEach((doc) => {
   data = doc.data();
-})
+});
 
 export const FormPopUp = (props) => {
   const show = props.showPopUp;
@@ -16,7 +19,7 @@ export const FormPopUp = (props) => {
   if (show) {
     return (
       <div className="h-screen bg-slate-900 z-10 fixed top-0 w-full left-0 bg-opacity-80 flex justify-center items-center">
-        <div className="w-[40%] bg-white text-xl rounded overflow-hidden">
+        <div className="w-full mx-[5%] sm:w-[80%] md:w-[60%] lg:w-[40%] bg-white text-xl rounded overflow-hidden">
           <div className="bg-blue-500 py-2 w-full flex justify-end pr-5 text-white">
             <div
               onClick={() => {
@@ -24,20 +27,41 @@ export const FormPopUp = (props) => {
               }}
               className="cursor-pointer"
             >
-              X
+              <AiOutlineClose />
             </div>
           </div>
-          <p className="px-10 py-5">
-            {
-              isError
-              ? "Registrasi Anda Gagal! Silahkan coba lagi!"
-              : "Data Anda berhasil dikirimkan, Terima Kasih!"
-            }
-          </p>
-          <div className="">
-          <p>Silahkan bergabung di grup whatsapp ini</p>
-          <a href={data.inv}> {data.inv} </a>
-        </div>
+          <div className="text-center py-5">
+            {isError ? (
+              <div
+                className="flex justify-center text-[100px] text-red-500"
+                data-aos="flip-left"
+              >
+                <IoMdCloseCircle />
+              </div>
+            ) : (
+              <div
+                className="flex justify-center text-[100px] text-green-300"
+                data-aos="flip-left"
+                data-aos-delay="50"
+              >
+                <FaCheckCircle />
+              </div>
+            )}
+            <p className="px-10 py-5" data-aos="fade-right" data-aos-delay="100">
+              {isError
+                ? "Registrasi Anda Gagal! Silahkan coba lagi!"
+                : "Data Anda berhasil dikirimkan, Terima Kasih!"}
+            </p>
+            {isError ? null : (
+              <>
+                <p className="px-10" data-aos="fade-right" data-aos-delay="150">Silahkan bergabung di grup whatsapp ini</p>
+                <a className="text-blue-400" href={data.inv} data-aos="fade-right" data-aos-delay="200">
+                  {" "}
+                  {data.inv}{" "}
+                </a>
+              </>
+            )}
+          </div>
         </div>
       </div>
     );
