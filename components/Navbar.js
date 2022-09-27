d413b3f93ddd41f00570e9b7cda2426d46e82be9
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2022-09-27 20:33:21 +0700

    fix: navbar responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -21,13 +21,13 @@ function Navbar() {
       <div className="px-4 py-4 bg-white border-b-4">
         <div className="container mx-auto font-Josefin">
           <div className="flex items-center">
-            <div className="w-3/12 text-3xl font-semibold">
+            <div className="w-3/12 md:3/12 text-3xl font-semibold">
               <Link href="/">
                 <Image src={logoOH} alt="logo" width="80" height="80" /> 
               </Link>
             </div>
             <div className="w-6/12">
-              <ul className="flex justify-center space-x-10">
+              <ul className="flex justify-center space-x-10 invisible md:visible">
                     <li><a className="hover:text-blue-500 font-l text-bold text-l py-4"
                       onClick={() => {
                         scrollToEvents();
@@ -43,7 +43,7 @@ function Navbar() {
                     </a></li>
               </ul>
             </div>
-            <div className="w-3/12 text-right">
+            <div className="w-5/12 md:w-3/12 text-right">
               <Link href="/faq">
                 <a className="border-2 border-blue-500 text-blue-500 py-2 px-10 hover:bg-blue-500 hover:text-white rounded-full text-bold">
                   FAQ
