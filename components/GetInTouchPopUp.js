e192441c8c8fc0c39f4153f8a009eb3db7526806
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2022-09-27 20:02:31 +0700

    fix: improve getintouch handler

--- components/GetInTouchPopUp.js
+++ components/GetInTouchPopUp.js
@@ -1,3 +1,7 @@
+import { FaCheckCircle } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+import { IoMdCloseCircle } from "react-icons/io";
+
 export const GetInTouchPopUp = (props) => {
   const show = props.getInTouchPopUpShowPopUp;
   const isError = props.getInTouchPopUpSubmitError;
@@ -7,7 +11,7 @@ export const GetInTouchPopUp = (props) => {
   if (show) {
     return (
       <div className="h-screen bg-slate-900 z-10 fixed top-0 w-full left-0 bg-opacity-80 flex justify-center items-center">
-        <div className="w-[40%] bg-white text-xl rounded overflow-hidden">
+        <div className="w-full mx-[5%] sm:w-[80%] md:w-[60%] lg:w-[40%] bg-white text-xl rounded overflow-hidden">
           <div className="bg-blue-500 py-2 w-full flex justify-end pr-5 text-white">
             <div
               onClick={() => {
@@ -15,16 +19,37 @@ export const GetInTouchPopUp = (props) => {
               }}
               className="cursor-pointer"
             >
-              X
+              <AiOutlineClose />
             </div>
           </div>
-          <p className="px-10 py-5">
-            {
-              isError
-              ? "Pesan Anda gagal dikirimkan. Silahkan coba lagi!"
-              : "Pesan Anda berhasil dikirimkan, Silahkan tunggu balasan kami melalui e-mail!"
-            }
-          </p>
+          <div className="text-center py-5">
+            {isError ? (
+              <div
+                className="flex justify-center text-[100px] text-red-500"
+                data-aos="flip-left"
+              >
+                <IoMdCloseCircle />
+              </div>
+            ) : (
+              <div
+                className="flex justify-center text-[100px] text-green-300"
+                data-aos="flip-left"
+                data-aos-delay="50"
+              >
+                <FaCheckCircle />
+              </div>
+            )}
+            <p className="px-10 py-5" data-aos="fade-up" data-aos-delay="100">
+              {isError
+                ? "Registrasi Anda Gagal! Silahkan coba lagi!"
+                : "Pesan Anda berhasil dikirimkan!"}
+            </p>
+            {isError ? null : (
+              <>
+                <p className="px-10" data-aos="fade-up" data-aos-delay="150">Silahkan tunggu balasan dari kami!</p>
+              </>
+            )}
+          </div>
         </div>
       </div>
     );
