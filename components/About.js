9f29f44816b31c25e86cd652d6b9fa5044bf0036
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-09-28 20:15:03 +0700

    fix: responsiveness on About section

--- components/About.js
+++ components/About.js
@@ -2,14 +2,13 @@ import React from "react";
 
 export default function About() {
     return (
-        // TODO: Responsive (font size)
         <main className="overflow-x-hidden">
-        <div className="w-full h-full px-16 pt-[209px] pb-[175px] bg-white" id="about">
+        <div className="w-full h-full px-7 sm:px-10 md:px-16 pt-[209px] pb-[175px] bg-white" id="about">
             <div className="grid container mx-auto space-y-16 font-Poppins text-center">
-                <h2 className="text-[54px] font-bold text-black-100" data-aos="zoom-in" data-aos-duration="2000">
+                <h2 className="text-[40px] leading-tight sm:text-5xl sm:leading-tight font-bold text-black-100" data-aos="zoom-in" data-aos-duration="2000">
                     About Open House
                 </h2>
-                <p className="text-[27px] font-normal text-black-200" data-aos="zoom-in" data-aos-duration="2000">
+                <p className="text-xl leading-relaxed sm:text-2xl sm:leading-relaxed font-normal text-black-200" data-aos="zoom-in" data-aos-duration="2000">
                     Open House merupakan kegiatan tahunan yang diadakan oleh Departemen Teknik Elektro dan Teknologi Informasi 
                     yang bertujuan untuk memperkenalkan DTETI ke masyarakat luas khususnya kepada siswa/i yang akan 
                     melanjutkan studinya ke perguruan tinggi. Open House DTETI 2022 mengangkat tema TETIZONE 
