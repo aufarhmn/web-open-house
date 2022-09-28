6bd3d9384d83ae32e659ec6b138bcd691dd456bb
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-09-28 19:12:54 +0700

    fix: responsive on Teti Talk
    - minus external components (GetInTouch, Benefits)

--- components/TetiTalk/SpeakerCard.js
+++ components/TetiTalk/SpeakerCard.js
@@ -3,54 +3,45 @@ import Image from "next/image";
 //TODO: responsive
 export const SpeakerCard = (props) => {
   const benefitsStyle =
-    "bg-[#F3F3F3] rounded-[28px] w-[430px] h-[592px] flex flex-col items-center ";
+    "bg-[#F3F3F3] rounded-[28px] max-w-[430px] h-min flex flex-col items-center lg:w-max";
   const imgStyle =
-    "w-[100%] h-[100%] flex justify-center items-end rounded-t-[28px]";
+    "w-[100%] aspect-square flex justify-center items-end rounded-t-[28px]";
   const shadow = {
     boxShadow:
       "-13.1635px -13.1635px 32.9088px #FFFFFF, 13.1635px 13.1635px 32.9088px #D2D2D2",
   };
 
-  let flexDir = "w-[full] flex flex-col xl:flex-row my-3 xl:gap-20 gap-10 justify-center items-center";
+  let flexDir = "w-[full] flex flex-col lg:flex-row my-3 xl:gap-20 gap-4 justify-center items-center lg:items-start";
 
   if(props.index % 2 !== 1) {
-    flexDir = "w-[full] flex flex-col my-3 xl:gap-20 gap-10 justify-center xl:flex-row-reverse items-center";
+    flexDir = "w-[full] flex flex-col my-3 xl:gap-20 gap-4 justify-center lg:flex-row-reverse items-center lg:items-start";
   } 
 
   return (
-    <div className="w-[90%] mx-auto mb-32">
+    <div className="w-[90%] mx-[5%] sm:mx-auto mb-32">
       <div className={flexDir}>
-        <div className={benefitsStyle} style={shadow} data-aos="fade-up" data-aos-duration="2000">
+        <div className={benefitsStyle} style={shadow} data-aos="fade-up" data-aos-duration="2000" data-aos-offset="-100">
           <div className={imgStyle}>
             <Image
               src={props.photo}
               alt="advantage1"
               width={400}
-              height={300}
+              height={400}
             />
           </div>
-          <div className="h-[35%] w-[80%] flex flex-col justify-center">
-            <p className="font-Josefin text-[26px] text-[#434343] font-bold">
+          <div className="h-[35%] w-[80%] flex flex-col justify-center py-5">
+            <p className="font-Josefin text-[20px] md:text-[26px] text-[#434343] font-bold">
               {props.name}
             </p>
-            <p className="font-Josefin text-[26px] text-[#434343]">
+            <p className="font-Josefin text-[20px] md:text-[26px] text-[#434343]">
               {props.job}
             </p>
           </div>
         </div>
 
-        <div className="max-w-[646px] font-Poppins flex flex-col justify-evenly" data-aos="fade-left" data-aos-duration="2000">
-          <h2 className="text-[32px] font-bold">{props.name}</h2>
-          <p className="text-[24.22px] mt-10">{props.desc}</p>
-          <p className="text-[24.22px] mt-10">
-            Best Paper Award, IEEE Indonesian Section, 2015 Best Paper Award,
-            Robotics Society of Taiwan, 2013
-          </p>
-          <div className="flex w-full gap-2">
-            <div className="bg-gray-500 h-[50px] w-full rounded"></div>
-            <div className="bg-gray-500 h-[50px] w-full rounded"></div>
-            <div className="bg-gray-500 h-[50px] w-full rounded"></div>
-          </div>
+        <div className="lg:max-w-[450px] xl:max-w-[646px] lg:mt-10 lg:mx-5 font-Poppins flex flex-col justify-evenly" data-aos="fade-left" data-aos-offset="-100" data-aos-duration="2000">
+          <h2 className="text-[24px] sm:text-[28px] font-bold">{props.name}</h2>
+          <p className="text-[18px] sm:text-[20px] md:text-[24.22px] mt-2">{props.desc}</p>
         </div>
       </div>
     </div>
--- pages/tetitalk.js
+++ pages/tetitalk.js
@@ -5,6 +5,7 @@ import { SpeakerCard } from "../components/TetiTalk/SpeakerCard";
 import Footer from "../components/Footer";
 import BackToTop from "../components/BackToTop";
 import GetInTouch from "../components/GetInTouch";
+import pakAdha from '../Assets/Images/Speaker/pakAdha.png'
 
 
 export default function TETITalk() {
@@ -15,19 +16,19 @@ export default function TETITalk() {
       index: 1,
       name: "Adha Imam Cahyadi",
       job: "Control for nonlinear dynamical systems, Control for systems with delay, Multi agents control systems, Teleoperation systems",
-      photo: "/vercel.svg",
+      photo: pakAdha,
     },
     {
       index: 2,
       name: "Adha Imam Cahyadi 2",
       job: "Control for nonlinear dynamical systems, Control for systems with delay, Multi agents control systems, Teleoperation systems",
-      photo: "/vercel.svg",
+      photo: pakAdha,
     },
     {
       index: 3,
       name: "Adha Imam Cahyadi 3",
       job: "Control for nonlinear dynamical systems, Control for systems with delay, Multi agents control systems, Teleoperation systems",
-      photo: "/vercel.svg",
+      photo: pakAdha,
     },
   ]
 
@@ -38,16 +39,16 @@ export default function TETITalk() {
       </Head>
       <main className="overflow-x-hidden">
         <NavbarSimplified />
-        <h1 className="text-[134px] font-Josefin font-bold my-[120px] mx-[100px] text-blue-25"
+        <h1 className="text-[60px] sm:text-[80px] md:text-[134px] w-max mx-auto font-Josefin font-bold :my-[120px] lg:mx-[100px] text-blue-25"
         data-aos="fade-up" data-aos-duration="2000">
           TETI Talk
         </h1>
 
-        <div className="flex px-32 font-Josefin flex-col xl:flex-row justify-between gap-4 text-green-200">
-          <div className="font-bold text-center" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="700">
-            <h2 className="text-[58.5px] w-max">About TETI</h2>
+        <div className="flex md:px-32 font-Josefin flex-col lg:flex-row justify-between lg:gap-4 lg:md-0 lg:items-center text-green-200">
+          <div className="font-bold text-center w-min mx-auto mt-10 sm:mt-5" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="700">
+            <h2 className="text-[48px] md:text-[58.5px] w-max">About TETI</h2>
             <h2
-              className="text-[72.65px] bg-[#E7FFC1] max-w-[313px]"
+              className="text-[60px] sm:text-[65px] md:text-[72.65px] bg-[#E7FFC1] max-w-[313px]"
               style={{
                 clipPath: "polygon(10% 0%, 100% 0%, 90% 100%, 0% 100%)",
               }}
@@ -55,7 +56,7 @@ export default function TETITalk() {
               Talk
             </h2>
           </div>
-          <p className="max-w-[730px] text-[30px] font-light" data-aos="fade-left" data-aos-duration="2000" data-aos-delay="700">
+          <p className="max-w-[730px] text-[22px] mx-[5%] lg:mx-0 lg:ml-10 sm:text-[22px] md:text-[26px] font-light" data-aos="fade-left" data-aos-duration="2000" data-aos-delay="700">
             Acara berupa talkshow yang akan mengundang alumni-alumni DTETI untuk berbincang bersama, berbagi insight serta pengalaman mereka setelah lulus. Pada Open House kali ini akan mengundang ...
           </p>
         </div>
@@ -63,11 +64,11 @@ export default function TETITalk() {
         <Benefits />
 
         <div className="mx-auto w-[80%]">
-          <h1 className="text-[58.5px] font-Poppins font-bold text-green-200 mb-32 text-center">
+          <h1 className="text-[40px] sm:text-[58.5px] font-Poppins font-bold text-green-200 mb-10 text-center">
             Meet Our Speakers
           </h1>
         </div>
-        <div className="odd:flex-row-reverse">
+        <div className="mx-[5%]">
           {speakers.map((speaker) => (
             <SpeakerCard index={speaker.index} photo={speaker.photo} name={speaker.name} job="Dosen" desc={speaker.job} key={speaker.index}/>
           ))}
