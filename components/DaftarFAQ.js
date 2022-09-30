7159d32a9ff259f1b6f337b330402c5f2d7785a9
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2022-09-30 14:31:13 +0700

    fix: hydration error while fetching data

--- components/DaftarFAQ.js
+++ components/DaftarFAQ.js
@@ -1,20 +1,28 @@
 import React from "react";
+import { useState } from "react";
 import { db } from "../firebase/firebase.js";
 import { collection, getDocs } from "firebase/firestore";
 
 const styleQ = "w-full px-[24px] py-[25px] rounded bg-grey-100 font-Josefin font-bold text-xl"
 const styleA = "font-Josefin font-light text-xl pb-10"
 
-const faqRef = collection(db, "faq-details");
-const docsSnap = await getDocs(faqRef);
-var data = docsSnap.docs.map((doc) => doc.data());
+const DaftarFAQ = () => {
+    const [faqData, setFaqData] = useState([]);
 
-export default function DaftarFAQ () {
+    const getFaq = async () => {
+        const faqRef = collection(db, "faq-details");
+        const docsSnap = await getDocs(faqRef);
+        var data = docsSnap.docs.map((doc) => doc.data());
+        setFaqData(data);
+    };
+
+    getFaq();
+
     return (
         <div className="w-full h-full grid content-center px-16 py-[100px] bg-white">
             <div className="container mx-auto space-y-5">
                 {
-                    data.map((faq, index) => {
+                    faqData.map((faq, index) => {
                         return (
                             <>
                                 <div className={styleQ} key={index}>
@@ -30,3 +38,5 @@ export default function DaftarFAQ () {
         </div>
     )
 }
+
+export default DaftarFAQ;
