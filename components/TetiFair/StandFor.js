8ebfe2972362bdedbf231f40a16cfc5aa450d539
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-09-30 19:45:13 +0700

    fix: update flex direction on StandFor section

--- components/TetiFair/StandFor.js
+++ components/TetiFair/StandFor.js
@@ -19,13 +19,12 @@ const styleDescription = "text-[17px] text-justify"
 
 export default function StandFor() {
     return (
-        // TODO: Responsive, aos
         <div className="w-full h-full px-7 md:px-16 pb-[120px]">
             <div className="grid container mx-auto space-y-20 md:space-y-32">
                 <h3 className="text-[58px] font-bold text-black-100 text-left font-Josefin" data-aos="fade-up" data-aos-duration="2000">
                     What Does TETITB Stand for?
                 </h3>
-                <div className="flex items-start justify-between flex-col lg:flex-row gap-y-20 xl:gap-y-0 gap-x-10 xl:gap-x-[62px] font-Montserrat text-black-200 text-center" data-aos="fade-up" data-aos-duration="2000">
+                <div className="flex items-start justify-between flex-col xl:flex-row gap-y-20 xl:gap-y-0 gap-x-10 xl:gap-x-[62px] font-Montserrat text-black-200 text-center" data-aos="fade-up" data-aos-duration="2000">
                     <div className="grid justify-items-center">
                         <div className={styleCircle} style={shadowCircle}>
                             <div className={styleCircleInner} style={shadowCircleInner}>
