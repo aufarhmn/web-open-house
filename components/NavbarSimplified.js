019b5cefd9e27c662bde05175b083a13165c04e7
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-10-02 12:28:17 +0700

    fix: update Navbar responsiveness

--- components/NavbarSimplified.js
+++ components/NavbarSimplified.js
@@ -8,18 +8,27 @@ function NavbarSimplified() {
     return (
         // TODO: Responsive (Breakpoint pakai hamburger icon) + Fixed Content
         <>
-            <div className="px-4 py-4 bg-white border-b-4">
-                <div className="container mx-auto font-Josefin">
-                    <div className="flex items-center">
-                        <div className="w-6/12 text-3xl font-semibold">
-                            <Link href="/">
-                                <Image src={logoOH} alt="logo" width="80" height="80" />
-                            </Link>
-                        </div>
-                        <div className="w-6/12 text-right">
-                            <Link href="/">
-                                <a className="border-2 border-blue-500 text-blue-500 py-2 px-10 hover:bg-blue-500 hover:text-white rounded-full text-bold">Home</a>
-                            </Link>
+            <div className="overflow-x-hidden">
+                <div className="px-4 sm:px-6 md:px-16 py-2 bg-white border-b-2 md:border-b-4">
+                    <div className="md:container mx-auto font-Josefin">
+                        <div className="flex items-center">
+                            <div className="w-6/12">
+                                <Link href="/">
+                                    <Image 
+                                        src={logoOH} 
+                                        alt="logo" 
+                                        width="80" 
+                                        height="80" 
+                                    />
+                                </Link>
+                            </div>
+                            <div className="w-6/12 text-right">
+                                <Link href="/">
+                                    <a className="border-2 border-blue-500 text-lg md:text-xl text-blue-500 py-1.5 sm:py-2 px-6 sm:px-10 hover:bg-blue-500 hover:text-white rounded-full">
+                                        Home
+                                    </a>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
